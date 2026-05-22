e08b79d82b8a56e7bf0795bf4ec0c9f7036f4727
Krishav Raj Singh <krishavrajsingh@gmail.com>
2026-05-22 09:38:47 +0530

    favicons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react"
+import Head from "next/head"
 import type { AppProps } from "next/app"
 
 import "~components/landing/landing.css"
@@ -7,6 +8,11 @@ import "~style.css"
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <link href="/favicon.ico" rel="icon" sizes="any" />
+        <link href="/favicon.png" rel="icon" type="image/png" sizes="32x32" />
+        <link href="/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180" />
+      </Head>
       <Component {...pageProps} />
       <Analytics />
     </>
